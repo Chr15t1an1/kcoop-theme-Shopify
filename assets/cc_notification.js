c5bd9fe3a9b674d190f632aad2dcb27847a084c8
Chr15t1an <ccampbell@sapphirebd.com>
2018-02-10 13:28:26 -0500

    added aditional event listener.

--- assets/cc_notification.js
+++ assets/cc_notification.js
@@ -107,3 +107,8 @@ function cc_shop_checkCart(){
   $( document ).ready(cc_shop_checkCart);
 
   $( document ).ajaxStart(cc_shop_checkCart);
+
+  $("#AddToCart").click(function(){
+    cc_shop_checkCart();
+
+  });
